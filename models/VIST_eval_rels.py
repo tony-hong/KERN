29fd4f44fad9f892965ef4bf6dc4c0983e7ef2e0
Xudong Hong <xhong@coli.uni-saarland.de>
2020-01-30 16:08:53 +0000

    feat: implement graph pred for VIST

--- models/VIST_eval_rels.py
+++ models/VIST_eval_rels.py
@@ -22,7 +22,7 @@ ind_to_predicates = train.ind_to_predicates # ind_to_predicates[0] means no rela
 
 if conf.test:
     val = test
-_, val_loader = VISTDataLoader.splits(val, test, mode='rel',
+_, val_loader = VISTDataLoader.splits(val, test, mode='det',
                                                batch_size=conf.batch_size,
                                                num_workers=conf.num_workers,
                                                num_gpus=conf.num_gpus)
@@ -59,20 +59,12 @@ if conf.mode == 'sgdet':
 
 all_pred_entries = []
 
-def val_batch(batch_num, b, evaluator, evaluator_multiple_preds, evaluator_list, evaluator_multiple_preds_list, thrs=(20, 50, 100)):
+def val_batch(batch_num, b): 
     det_res = detector[b]
     if conf.num_gpus == 1:
         det_res = [det_res]
 
     for i, (boxes_i, objs_i, obj_scores_i, rels_i, pred_scores_i) in enumerate(det_res):
-        gt_entry = {
-            'gt_classes': val.gt_classes[batch_num + i].copy(),
-            'gt_relations': val.relationships[batch_num + i].copy(),
-            'gt_boxes': val.gt_boxes[batch_num + i].copy(),
-        }
-        assert np.all(objs_i[rels_i[:,0]] > 0) and np.all(objs_i[rels_i[:,1]] > 0)
-        # assert np.all(rels_i[:,2] > 0)
-
         pred_entry = {
             'pred_boxes': boxes_i * BOX_SCALE/IM_SCALE,
             'pred_classes': objs_i,
@@ -81,53 +73,12 @@ def val_batch(batch_num, b, evaluator, evaluator_multiple_preds, evaluator_list,
             'rel_scores': pred_scores_i,
         }
         all_pred_entries.append(pred_entry)
-
-        eval_entry(conf.mode, gt_entry, pred_entry, evaluator, evaluator_multiple_preds, 
-                   evaluator_list, evaluator_multiple_preds_list)
-
-
-evaluator = BasicSceneGraphEvaluator.all_modes()
-evaluator_multiple_preds = BasicSceneGraphEvaluator.all_modes(multiple_preds=True)
-evaluator_list = [] # for calculating recall of each relationship except no relationship
-evaluator_multiple_preds_list = []
-for index, name in enumerate(ind_to_predicates):
-    if index == 0:
-        continue
-    evaluator_list.append((index, name, BasicSceneGraphEvaluator.all_modes()))
-    evaluator_multiple_preds_list.append((index, name, BasicSceneGraphEvaluator.all_modes(multiple_preds=True)))
-
-if conf.cache is not None and os.path.exists(conf.cache):
-    print("Found {}! Loading from it".format(conf.cache))
-    with open(conf.cache,'rb') as f:
-        all_pred_entries = pkl.load(f)
-    for i, pred_entry in enumerate(tqdm(all_pred_entries)):
-        gt_entry = {
-            'gt_classes': val.gt_classes[i].copy(),
-            'gt_relations': val.relationships[i].copy(),
-            'gt_boxes': val.gt_boxes[i].copy(),
-        }
-
-        eval_entry(conf.mode, gt_entry, pred_entry, evaluator, evaluator_multiple_preds, 
-                   evaluator_list, evaluator_multiple_preds_list)
-
-    recall = evaluator[conf.mode].print_stats()
-    recall_mp = evaluator_multiple_preds[conf.mode].print_stats()
     
-    mean_recall = calculate_mR_from_evaluator_list(evaluator_list, conf.mode, save_file=conf.save_rel_recall)
-    mean_recall_mp = calculate_mR_from_evaluator_list(evaluator_multiple_preds_list, conf.mode, multiple_preds=True, save_file=conf.save_rel_recall)
+detector.eval()
+for val_b, batch in enumerate(tqdm(val_loader)):
+    val_batch(conf.num_gpus*val_b, batch)
 
-else:
-    detector.eval()
-    for val_b, batch in enumerate(tqdm(val_loader)):
-        val_batch(conf.num_gpus*val_b, batch, evaluator, evaluator_multiple_preds, evaluator_list, evaluator_multiple_preds_list)
-
-    recall = evaluator[conf.mode].print_stats()
-    recall_mp = evaluator_multiple_preds[conf.mode].print_stats()
-    
-    mean_recall = calculate_mR_from_evaluator_list(evaluator_list, conf.mode, save_file=conf.save_rel_recall)
-    mean_recall_mp = calculate_mR_from_evaluator_list(evaluator_multiple_preds_list, conf.mode, multiple_preds=True, save_file=conf.save_rel_recall)
-
-    if conf.cache is not None:
-        with open(conf.cache,'wb') as f:
-            pkl.dump(all_pred_entries, f)
+if conf.cache is not None:
+    with open(conf.cache,'wb') as f:
+        pkl.dump(all_pred_entries, f)
 
